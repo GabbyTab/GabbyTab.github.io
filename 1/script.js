d38d6badaec58be35058a7ebadc8d68bf6c20708
GabbyTab <114787493+GabbyTab@users.noreply.github.com>
2024-09-06 15:21:11 -0300

    Update script.js: multiple images

--- 1/script.js
+++ 1/script.js
@@ -2,14 +2,14 @@ document.addEventListener('scroll', function() {
     const header = document.getElementById('header');
     const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
     const height = header.offsetHeight;
-    
+
     console.log(`scrollTop: ${scrollTop}, height: ${height}`);
     
     if (scrollTop < height / 3) {
         header.style.backgroundImage = "url('images/color_out_lady.png?" + new Date().getTime() + "')";
     } else if (scrollTop < height / 2) {
-       header.style.backgroundImage = "url('images/color_out_lady.png?" + new Date().getTime() + "')";
+        header.style.backgroundImage = "url('images/contrast_out_lady.png?" + new Date().getTime() + "')";
     } else {
-        header.style.backgroundImage = "url('images/color_out_lady.png?" + new Date().getTime() + "')";
+        header.style.backgroundImage = "url('images/test_lady.png?" + new Date().getTime() + "')";
     }
 });
