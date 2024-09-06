3216637234ef528b36b37043e4d9b59f638e232d
GabbyTab <114787493+GabbyTab@users.noreply.github.com>
2024-09-06 15:14:02 -0300

    Update script.js: possible css cash issue fix

--- 1/script.js
+++ 1/script.js
@@ -6,10 +6,10 @@ document.addEventListener('scroll', function() {
     console.log(`scrollTop: ${scrollTop}, height: ${height}`);
     
     if (scrollTop < height / 3) {
-        header.style.backgroundImage = "url('images/color_out_lady.png')";
+        header.style.backgroundImage = "url('images/color_out_lady.png?" + new Date().getTime() + "')";
     } else if (scrollTop < height / 2) {
-        header.style.backgroundImage = "url('images/contrast_out_lady.png')";
+       header.style.backgroundImage = "url('images/color_out_lady.png?" + new Date().getTime() + "')";
     } else {
-        header.style.backgroundImage = "url('images/test_lady.png')";
+        header.style.backgroundImage = "url('images/color_out_lady.png?" + new Date().getTime() + "')";
     }
 });
