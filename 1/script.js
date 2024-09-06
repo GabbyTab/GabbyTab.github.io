1a2c38cf4e40be40e045aded0e366c313e583dd5
GabbyTab <114787493+GabbyTab@users.noreply.github.com>
2024-09-06 15:12:56 -0300

    Update script.js: Console Log

--- 1/script.js
+++ 1/script.js
@@ -3,9 +3,11 @@ document.addEventListener('scroll', function() {
     const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
     const height = header.offsetHeight;
     
-    if (scrollTop < height/3) {
+    console.log(`scrollTop: ${scrollTop}, height: ${height}`);
+    
+    if (scrollTop < height / 3) {
         header.style.backgroundImage = "url('images/color_out_lady.png')";
-    } else if (scrollTop < height/2) {
+    } else if (scrollTop < height / 2) {
         header.style.backgroundImage = "url('images/contrast_out_lady.png')";
     } else {
         header.style.backgroundImage = "url('images/test_lady.png')";
