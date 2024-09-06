c0528ccdab2b8b42970048a9004749ae028d14f6
GabbyTab <114787493+GabbyTab@users.noreply.github.com>
2024-09-06 14:59:21 -0300

    Update script.js

--- 1/script.js
+++ 1/script.js
@@ -4,10 +4,10 @@ document.addEventListener('scroll', function() {
     const height = header.offsetHeight;
     
     if (scrollTop < height) {
-        header.style.backgroundImage = "url('images/color_out_lady.jpg')";
+        header.style.backgroundImage = "url('images/color_out_lady.png')";
     } else if (scrollTop < 2 * height) {
-        header.style.backgroundImage = "url('images/contrast_out_lady.jpg')";
+        header.style.backgroundImage = "url('images/contrast_out_lady.png')";
     } else {
-        header.style.backgroundImage = "url('images/test_lady.jpg')";
+        header.style.backgroundImage = "url('images/test_lady.png')";
     }
 });
